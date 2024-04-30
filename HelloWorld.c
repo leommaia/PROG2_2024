30aecdeb21d618bf9f146687a802f7b2e2c9d4e7
Leonardo Maia <Leonardo Maia@DESKTOP-H4MH53F>
2024-04-30 16:06:06 -0300

    .

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+// Função para chamar um "Hello World"
+
+void imprimeTexto();
+
 int main(){
 
-printf("Hello World!");
+// printf("Hello World!");
 
 return 0;
+
+}
+
+void imprimeTexto() {
+
+	printf("Hello World!");
 
 }
